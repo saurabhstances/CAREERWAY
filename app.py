f0ad01433ac062e23a0771aaad57c71c12250b35
Saurabh S Gupta <saurabhgupta33943@gmail.com>
2026-07-17 13:18:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,8 @@ class StudyLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     date = db.Column(db.String(20), nullable=False) # Format YYYY-MM-DD
-   status = db.Column(db.String(20), default='Completed')
-        topic_covered = db.Column(db.String(100))
+    status = db.Column(db.String(20), default='Completed')
+    topic_covered = db.Column(db.String(100))
 
            
 # --- DATABASE SETUP ---
